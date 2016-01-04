c0e80406113d63e362f7f22402fdd764bce14bae
Fabien Ruffin <fabien.ruffin@outlook.com>
2016-01-04 13:12:23 +1100

    Fixed ExpirationManager by reversing the "IsAssignableFrom" call in the in-memory database

--- Database/Data.cs
+++ Database/Data.cs
@@ -14,44 +14,44 @@ namespace Hangfire.MemoryStorage.Database
 
         public static IEnumerable<T> GetEnumeration<T>()
         {
-            var dict = GetDictionary(typeof (T));
+            var dict = GetDictionary(typeof(T));
             return dict.Values.Cast<T>();
         }
 
         public static IEnumerable<IExpirable> GetExpirables(Type type)
         {
-            if (!type.IsAssignableFrom(typeof (IExpirable)))
+            if (!typeof(IExpirable).IsAssignableFrom(type))
                 return new List<IExpirable>();
 
             var dict = GetDictionary(type);
-            return (IEnumerable<IExpirable>) dict.Values;
+            return (IEnumerable<IExpirable>)dict.Values;
         }
 
         public static T GetOrCreate<T>(string key, T element)
         {
-            var dict = GetDictionary(typeof (T));
-            return (T) dict.GetOrAdd(key, element);
+            var dict = GetDictionary(typeof(T));
+            return (T)dict.GetOrAdd(key, element);
         }
 
         public static T Get<T>(string key) where T : class
         {
             object obj = null;
-            var dict = GetDictionary(typeof (T));
+            var dict = GetDictionary(typeof(T));
             dict.TryGetValue(key, out obj);
-            return (T) obj;
+            return (T)obj;
         }
 
         public static T Get<T>(int key) where T : class
         {
             object obj = null;
-            var dict = GetDictionary(typeof (T));
+            var dict = GetDictionary(typeof(T));
             dict.TryGetValue(key, out obj);
-            return (T) obj;
+            return (T)obj;
         }
 
         internal static void Create<T>(IEnumerable<IIntIdentifiedData> elements)
         {
-            var dict = GetDictionary(typeof (T));
+            var dict = GetDictionary(typeof(T));
             foreach (var element in elements)
             {
                 dict.TryAdd(element.Id, element);
@@ -60,18 +60,18 @@ namespace Hangfire.MemoryStorage.Database
 
         public static T Create<T>(IIntIdentifiedData element)
         {
-            var dict = GetDictionary(typeof (T));
+            var dict = GetDictionary(typeof(T));
             dict.TryAdd(element.Id, element);
 
-            return (T) element;
+            return (T)element;
         }
 
         public static T Create<T>(IStringIdentifiedData element)
         {
-            var dict = GetDictionary(typeof (T));
+            var dict = GetDictionary(typeof(T));
             dict.TryAdd(element.Id, element);
 
-            return (T) element;
+            return (T)element;
         }
 
         public static void Delete(Type type, IIntIdentifiedData element)
