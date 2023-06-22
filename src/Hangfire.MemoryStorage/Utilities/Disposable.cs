34fc5db6501741edef4ed962c1998a1a855ac22b
Timo Witte <timo.witte@gmail.com>
2023-06-23 00:56:45 +0200

    return the more generic IDisposable in Disposable.Create

--- src/Hangfire.MemoryStorage/Utilities/Disposable.cs
+++ src/Hangfire.MemoryStorage/Utilities/Disposable.cs
@@ -4,13 +4,13 @@ namespace Hangfire.MemoryStorage.Utilities
 {
     public class Disposable : IDisposable
     {
-        public static Disposable Create(Action onDispose)
+        public static IDisposable Create(Action onDispose)
         {
             return new Disposable { OnDispose = onDispose };
         }
 
         private Disposable() { }
-        
+
         private Action OnDispose { get; set; }
         
         public void Dispose()
